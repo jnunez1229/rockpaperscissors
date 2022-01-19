f9e18c90e176ec89ed93ee6bc8cdadef3e75707b
jnunez1229 <thatjuanguy89@gmail.com>
2022-01-18 21:03:08 -0600

    moved score into play_round, finished play_round funciton (minus score issue)

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import inquirer
 
 
 def game():
-    # score
-    user_score = 0
-    robot_score = 0
-
     def user_selection():
         # User Selection
         user_prompt = [
@@ -17,7 +13,7 @@ def game():
         ]
         user_choice = inquirer.prompt(user_prompt)
 
-        #User answer is saved as a dict. Must convert to string
+        # User answer is saved as a dict. Must convert to string
         return str(user_choice['choice'])
 
     def robot_selection():
@@ -34,13 +30,8 @@ def game():
 
         return robot_choice
 
-    def play_round():
-        user_choice = user_selection()
-        robot_choice = robot_selection()
+        # User chooses if they want to continue after conclusion of game
 
-        print(user_choice + " - " + robot_choice)
-
-    # User chooses if they want to continue after conclusion of game
     def replay():
         user_prompt = [
             inquirer.List('choice',
@@ -55,17 +46,49 @@ def game():
         else:
             print('Ok, Thanks for playing!')
 
-    # After overall winner decided
-    if user_score > 3:
-        print("You win the game!!")
+    def play_round():
         user_score = 0
         robot_score = 0
-        replay()
-    elif robot_score > 3:
-        print("You lose =(")
-        user_score = 0
-        robot_score = 0
-        replay()
+
+        user_choice = user_selection()
+        robot_choice = robot_selection()
+
+        print("Robot chooses - " + robot_choice)
+
+        if user_choice == robot_choice:
+            print("Tie. No points")
+            play_round()
+        elif (user_choice == "Rock" and robot_choice == "Scissors") or (
+                user_choice == "Paper" and robot_choice == "Rock") or (
+                user_choice == "Scissors" and robot_choice == "Paper"):
+            print(user_choice + " beats " + robot_choice + "! You win this round! ")
+            user_score += 1
+
+            print("Your score: " + str(user_score))
+            print("Robot's score: " + str(robot_score))
+
+            play_round()
+        else:
+            print(robot_choice + " beats " + user_choice + ". You lose this round ")
+            robot_score += 1
+
+            print("Your score: " + str(user_score))
+            print("Robot's score: " + str(robot_score))
+
+            play_round()
+
+        # After overall winner decided
+        if user_score > 3:
+            print("You win the game!!")
+            user_score = 0
+            robot_score = 0
+            replay()
+        elif robot_score > 3:
+            print("You lose =(")
+            user_score = 0
+            robot_score = 0
+            replay()
+
     play_round()
 
 
