92f5ddbd082191903a37d0f26b6046c8f99d862a
jnunez1229 <thatjuanguy89@gmail.com>
2022-01-18 18:05:20 -0600

    created user selection

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from random import randint
+import inquirer
 
 
-def game(myChoice):
-    userCounter = 0
-    robotCounter = 0
-    robotChoice = randint(1,3)
-    print(robotChoice)
+def game():
+    # User Selection
+    userPrompt = [
+        inquirer.List('choice',
+                      message="Rock, paper, or scissors?",
+                      choices=['Rock', 'Paper', 'Scissors'],
+                      ),
+    ]
+    userChoice = inquirer.prompt(userPrompt)
 
-game("I win")
+    print(userChoice["choice"])
+
+    # if userChoice == 1 or userChoiceInt > 3 or userChoiceInt == ValueError:
+    #     print("you chose paper", userChoiceInt)
+    #     game()
+    # userScore = 0
+    # robotScore = 0
+    # robotChoice = randint(1, 3)
+    # print(userChoice)
+
+
+game()
